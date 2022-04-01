4a06d477c5c5f7169af42c3db7617425acb087b6
therealazimbek <aaazim04012002@gmail.com>
2022-04-01 16:43:33 +0600

    base

--- CodeHelp/src/app/app.module.ts
+++ CodeHelp/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { QuestionsComponent } from './components/questions/questions.component';
+import { QuestionDetailComponent } from './components/question-detail/question-detail.component';
+import { UserPageComponent } from './components/user-page/user-page.component';
+import { NotFoundPageComponent } from './components/not-found-page/not-found-page.component';
+import { AboutComponent } from './components/about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    QuestionsComponent,
+    QuestionDetailComponent,
+    UserPageComponent,
+    NotFoundPageComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
--- CodeHelp/src/test.js
+++
@@ -1,2 +0,0 @@
-console.log('HI')
-console.log('Bye')
